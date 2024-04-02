83261e2a06e97703479056ba8a4d492a6a2d607a
Brancen Gregory <brancengregory@gmail.com>
2024-04-01 23:20:48 -0500

    Init benchmark

---
+++ R/main.R
@@ -0,0 +1,54 @@
+library(bench)
+library(stringr)
+library(stringi)
+library(dplyr)
+library(gt)
+library(ggplot2)
+
+wordlist <- state.name
+regex_expression <- "(?i)ma"
+
+bench1 <- bench::mark(
+  grepl(regex_expression, wordlist),
+  str_detect(wordlist, regex_expression),
+  stri_detect(wordlist, regex = regex_expression),
+  stri_detect_regex(wordlist, regex_expression),
+  iterations = 10000
+)
+
+regex_expression <- "ma"
+
+bench2 <- bench::mark(
+  grepl(regex_expression, wordlist, ignore.case = TRUE),
+  str_detect(wordlist, regex(regex_expression, ignore_case = TRUE)),
+  stri_detect(wordlist, regex = regex_expression, case_insensitive = TRUE),
+  stri_detect_regex(wordlist, regex_expression, opts_regex = list(case_insensitive = TRUE)),
+  iterations = 10000
+)
+
+res <- bind_rows(
+  bench1 |>
+    mutate(case_insensitive_method = "regex"),
+  bench2 |>
+    mutate(case_insensitive_method = "function")
+)
+
+res |>
+  summarise(
+    .by = "case_insensitive_method",
+    median = median(median) |>
+      as.numeric()
+  ) |>
+  ggplot(aes(x = case_insensitive_method, y = median)) +
+    geom_col()
+
+res |>
+  arrange(desc(`itr/sec`), mem_alloc) |>
+  select(expression, case_insensitive_method, min, median, `itr/sec`, mem_alloc, `gc/sec`, total_time) |>
+  gt()
+
+data <- tibble(
+  state = wordlist
+)
+
+
